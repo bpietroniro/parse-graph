4eb9bf440f98575ff24d6c171187294992c0d7e4
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-04-14 12:00:05 -0400

    separated baseline structs into data module and wrote initial logic for parsing XML

---
+++ data/models.go
@@ -0,0 +1,20 @@
+package data
+
+type Graph struct {
+	ID    string `xml:"id"`
+	Name  string `xml:"name"`
+	Nodes []Node `xml:"nodes>node"`
+	Edges []Edge `xml:"edges>node"`
+}
+
+type Node struct {
+	ID   string `xml:"id"`
+	Name string `xml:"name"`
+}
+
+type Edge struct {
+	ID     string  `xml:"id"`
+	FromID string  `xml:"from"`
+	ToID   string  `xml:"to"`
+	Cost   float64 `xml:"cost"`
+}
--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
-type Graph struct {
-	ID    string
-	Name  string
-	Nodes []Node
-	Edges []Edge
-}
-
-type Node struct {
-	ID   string
-	Name string
-}
-
-type Edge struct {
-	ID     string
-	FromID string
-	ToID   string
-	Cost   float64
-}
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"parse-graph/data"
+)
 
 func main() {
+	filePath := "examples/graph_1.xml"
 
+	// Open the XML file
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Failed to open file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	// Read the file content
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Printf("Failed to read file: %v", err)
+		return
+	}
+
+	// Unmarshal the XML data into the struct
+	var graph data.Graph
+	err = xml.Unmarshal(content, &graph)
+	if err != nil {
+		fmt.Printf("Failed to parse XML: %v", err)
+		return
+	}
+
+	fmt.Println("XML parsing successful!")
+	fmt.Println(graph)
+	fmt.Println(graph.Nodes)
+	fmt.Println(graph.Edges)
 }
